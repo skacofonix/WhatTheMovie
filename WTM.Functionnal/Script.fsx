d404142606e70daaf387a74c8b1c0315412d33a0
Skacofonix <skacofonix@gmail.com>
2014-10-07 08:44:33 +0200

    Add F# project

---
+++ WTM.Functionnal/Script.fsx
@@ -0,0 +1,6 @@
+﻿// En savoir plus sur F# sur http://fsharp.net. Consultez le projet 'Didacticiel F#'
+// pour obtenir une aide supplémentaire sur la programmation F#.
+
+#load "PortableLibrary1.fs"
+open WTM.Functionnal
+
