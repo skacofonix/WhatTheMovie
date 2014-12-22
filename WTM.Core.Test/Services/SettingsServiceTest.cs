2b6f91aa9df0094ce78983e3fa15351ad4748313
Skacofonix <skacofonix@gmail.com>
2014-12-22 07:56:56 +0100

    Add SettingsService

---
+++ WTM.Core.Test/Services/SettingsServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using NFluent;
+using NUnit.Framework;
+using WTM.Core.Application;
+using WTM.Core.Domain.WebsiteEntities;
+using WTM.Core.Services;
+
+namespace WTM.Core.Test.Services
+{
+    [TestFixture]
+    public class SettingsServiceTest
+    {
+        private IWebClient webClient;
+        private IHtmlParser htmlParser;
+        private SettingsService settingsService;
+
+        [SetUp]
+        public void Init()
+        {
+            webClient = new WebClientWTM();
+            htmlParser = new HtmlParser();
+            settingsService = new SettingsService(webClient, htmlParser);
+        }
+
+        [Test]
+        public void WhenReadSettingsThenReturnInstance()
+        {
+            Check.That(settingsService.Read()).IsNotNull();
+        }
+
+        [Test]
+        public void WhenWriteSettingsThenReturnTrue()
+        {
+            var settings = new Settings();
+            Check.That(settingsService.Write(settings)).IsTrue();
+        }
+    }
+}
---
+++ WTM.Core/Services/SettingsService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using WTM.Core.Application;
+using WTM.Core.Application.Parsers;
+using WTM.Core.Domain.WebsiteEntities;
+
+namespace WTM.Core.Services
+{
+    internal class SettingsService
+    {
+        private readonly IWebClient webClient;
+        private readonly IHtmlParser htmlParser;
+        private readonly SettingsParser settingsParser;
+
+        public SettingsService(IWebClient webClient, IHtmlParser htmlParser)
+        {
+            this.webClient = webClient;
+            this.htmlParser = htmlParser;
+            settingsParser = new SettingsParser(webClient, htmlParser);
+        }
+
+        public Settings Read()
+        {
+            return settingsParser.Parse();
+        }
+
+        public bool Write(Settings settings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
