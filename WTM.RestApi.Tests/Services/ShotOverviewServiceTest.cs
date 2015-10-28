f5017ef22bd0d07cf661c7ce16197a69e45b8b30
Yannick Valy <yannick.valy@gmail.com>
2015-10-28 12:43:05 +0100

    Implement ShotOverviewServiceTest

--- WTM.RestApi.Tests/Services/ShotOverviewServiceTest.cs
+++ WTM.RestApi.Tests/Services/ShotOverviewServiceTest.cs
@@ -1,10 +1,10 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Moq;
 using NFluent;
 using NUnit.Framework;
 using WTM.Crawler.Services;
 using WTM.RestApi.Services;
-using ShotOverviewService = WTM.RestApi.Services.ShotOverviewService;
 
 namespace WTM.RestApi.Tests.Services
 {
@@ -12,7 +12,7 @@ namespace WTM.RestApi.Tests.Services
     public class ShotOverviewServiceTest
     {
         [Test]
-        public void ShouldSomething()
+        public void ShouldFindByDate()
         {
             var shotOverviewCrawler = new Mock<WTM.Crawler.Services.ShotOverviewService>();
             var shotFeatureFilmsCrawler = new Mock<ShotFeatureFilmsService>();
@@ -20,9 +20,73 @@ namespace WTM.RestApi.Tests.Services
             var shotNewSubmissionsService = new Mock<ShotNewSubmissionsService>();
             var dateTimeServie = new Mock<IDateTimeService>();
 
-            var shotOverviewService = new ShotOverviewService(shotOverviewCrawler.Object, shotFeatureFilmsCrawler.Object, shotArchiveCrawler.Object, shotNewSubmissionsService.Object, dateTimeServie.Object);
+            var shotOverviewService = new WTM.RestApi.Services.ShotOverviewService(shotOverviewCrawler.Object, shotFeatureFilmsCrawler.Object, shotArchiveCrawler.Object, shotNewSubmissionsService.Object, dateTimeServie.Object);
 
             var shotOverviewResponses = shotOverviewService.FindByDate(null, null, null);
+
+            Check.That(shotOverviewResponses.Any()).IsTrue();
+        }
+
+        [Test]
+        public void ShouldFindByTag()
+        {
+            var shotOverviewCrawler = new Mock<WTM.Crawler.Services.ShotOverviewService>();
+            var shotFeatureFilmsCrawler = new Mock<ShotFeatureFilmsService>();
+            var shotArchiveCrawler = new Mock<ShotArchiveService>();
+            var shotNewSubmissionsService = new Mock<ShotNewSubmissionsService>();
+            var dateTimeServie = new Mock<IDateTimeService>();
+
+            var shotOverviewService = new WTM.RestApi.Services.ShotOverviewService(shotOverviewCrawler.Object, shotFeatureFilmsCrawler.Object, shotArchiveCrawler.Object, shotNewSubmissionsService.Object, dateTimeServie.Object);
+
+            var shotOverviewResponses = shotOverviewService.FindByTag(new List<string> {"un"}, null, null);
+
+            Check.That(shotOverviewResponses.Any()).IsTrue();
+        }
+
+        [Test]
+        public void ShouldGetArchives()
+        {
+            var shotOverviewCrawler = new Mock<WTM.Crawler.Services.ShotOverviewService>();
+            var shotFeatureFilmsCrawler = new Mock<ShotFeatureFilmsService>();
+            var shotArchiveCrawler = new Mock<ShotArchiveService>();
+            var shotNewSubmissionsService = new Mock<ShotNewSubmissionsService>();
+            var dateTimeServie = new Mock<IDateTimeService>();
+
+            var shotOverviewService = new WTM.RestApi.Services.ShotOverviewService(shotOverviewCrawler.Object, shotFeatureFilmsCrawler.Object, shotArchiveCrawler.Object, shotNewSubmissionsService.Object, dateTimeServie.Object);
+
+            var shotOverviewResponses = shotOverviewService.GetArchives(null, null, null);
+
+            Check.That(shotOverviewResponses.Any()).IsTrue();
+        }
+
+        [Test]
+        public void ShouldGetFeatureFilms()
+        {
+            var shotOverviewCrawler = new Mock<WTM.Crawler.Services.ShotOverviewService>();
+            var shotFeatureFilmsCrawler = new Mock<ShotFeatureFilmsService>();
+            var shotArchiveCrawler = new Mock<ShotArchiveService>();
+            var shotNewSubmissionsService = new Mock<ShotNewSubmissionsService>();
+            var dateTimeServie = new Mock<IDateTimeService>();
+
+            var shotOverviewService = new WTM.RestApi.Services.ShotOverviewService(shotOverviewCrawler.Object, shotFeatureFilmsCrawler.Object, shotArchiveCrawler.Object, shotNewSubmissionsService.Object, dateTimeServie.Object);
+
+            var shotOverviewResponses = shotOverviewService.GetFeatureFilms(null, null, null);
+
+            Check.That(shotOverviewResponses.Any()).IsTrue();
+        }
+
+        [Test]
+        public void ShouldGetNewSubmissions()
+        {
+            var shotOverviewCrawler = new Mock<WTM.Crawler.Services.ShotOverviewService>();
+            var shotFeatureFilmsCrawler = new Mock<ShotFeatureFilmsService>();
+            var shotArchiveCrawler = new Mock<ShotArchiveService>();
+            var shotNewSubmissionsService = new Mock<ShotNewSubmissionsService>();
+            var dateTimeServie = new Mock<IDateTimeService>();
+
+            var shotOverviewService = new WTM.RestApi.Services.ShotOverviewService(shotOverviewCrawler.Object, shotFeatureFilmsCrawler.Object, shotArchiveCrawler.Object, shotNewSubmissionsService.Object, dateTimeServie.Object);
+
+            var shotOverviewResponses = shotOverviewService.GetNewSubmissions(null, null, null);
 
             Check.That(shotOverviewResponses.Any()).IsTrue();
         }
