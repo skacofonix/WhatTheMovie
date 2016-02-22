38d794f1cf853875f8f9e178f7c20fc2bc45c176
Yannick Valy <yannick.valy@gmail.com>
2016-02-22 20:17:05 +0100

    Add HttpRequestBuilderTest

---
+++ WTM.Crawler.Test/HttpRequestBuilderTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WTM.Crawler.Helpers;
+
+namespace WTM.Crawler.Test
+{
+    [TestFixture]
+    class HttpRequestBuilderTest
+    {
+        [Test]
+        public void ShoulMakeUrl()
+        {
+            var requestBuilder = new HttpRequestBuilder("prefix");
+            requestBuilder.AddParameter("username", "toto");
+            requestBuilder.AddParameter("password", "titi");
+            requestBuilder.AddParameter("duration", "20");
+            var result = requestBuilder.ToString();
+        }
+    }
+}
