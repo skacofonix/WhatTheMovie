0c8e856fd9afea3f3acbe2c085eb09d46f72db86
Yannick Valy <yannick.valy@gmail.com>
2016-02-23 13:49:25 +0100

    Include XML documentation into Swagger definition

--- WTM.Api/App_Start/SwaggerConfig.cs
+++ WTM.Api/App_Start/SwaggerConfig.cs
@@ -153,7 +153,7 @@ namespace WTM.Api
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        //c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // In contrast to WebApi, Swagger 2.0 does not include the query string component when mapping a URL
                         // to an action. As a result, Swashbuckle will raise an exception if it encounters multiple actions
@@ -214,6 +214,10 @@ namespace WTM.Api
                         //
                         //c.EnableOAuth2Support("test-client-id", "test-realm", "Swagger UI");
                     });
+        }
+        private static string GetXmlCommentsPath()
+        {
+            return $@"{System.AppDomain.CurrentDomain.BaseDirectory}\bin\WTM.Api.xml";
         }
     }
 }
