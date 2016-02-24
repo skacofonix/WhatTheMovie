0c53489179a1deb2b4a9a418a9532687c41f2040
Yannick Valy <yannick.valy@gmail.com>
2016-02-24 19:57:34 +0100

    Add missing file

---
+++ WTM.Api/Models/IUserSearchRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WTM.Api.Models
+{
+    public interface IUserSearchRequest : IRequest, IPaginableRequest
+    {
+        [Required]
+        string Filter { get; }
+
+        int? Start { get; }
+
+        int? Limit { get; }
+    }
+}
